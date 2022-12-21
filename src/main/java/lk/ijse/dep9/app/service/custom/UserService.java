ea5d6aece29cdc39386f7e601402f9fa19f115ab
Chathura Rupasinghe <chathura.rmc93@gmail.com>
2022-12-21 13:25:14 +0530

    refer connection pool inside the webXml

--- src/main/java/lk/ijse/dep9/app/service/custom/UserService.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/UserService.java
@@ -1,4 +1,10 @@
 package lk.ijse.dep9.app.service.custom;
 
-public interface UserService {
+import lk.ijse.dep9.app.dto.UserDTO;
+import lk.ijse.dep9.app.service.SuperService;
+
+public interface UserService extends SuperService {
+
+    void createNewUserAccount(UserDTO userDTO){
+    }
 }
