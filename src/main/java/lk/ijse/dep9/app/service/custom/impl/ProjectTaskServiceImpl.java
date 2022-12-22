f6421e6b7e0a5686e36b7d2a0a2dcb360749a76d
Chathura Rupasinghe <chathura.rmc93@gmail.com>
2022-12-22 15:59:10 +0530

    add AspectJ to the project

--- src/main/java/lk/ijse/dep9/app/service/custom/impl/ProjectTaskServiceImpl.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/impl/ProjectTaskServiceImpl.java
@@ -1,9 +1,71 @@
 package lk.ijse.dep9.app.service.custom.impl;
 
+import lk.ijse.dep9.app.dao.custom.ProjectDAO;
+import lk.ijse.dep9.app.dao.custom.TaskDAO;
+import lk.ijse.dep9.app.dto.ProjectDTO;
+import lk.ijse.dep9.app.entity.Project;
+import lk.ijse.dep9.app.exception.AccessDeniedException;
 import lk.ijse.dep9.app.service.custom.ProjectTaskService;
+import lk.ijse.dep9.app.util.Transformer;
 import org.springframework.context.annotation.Scope;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Transactional
 @Component
 public class ProjectTaskServiceImpl implements ProjectTaskService {
+
+    private ProjectDAO projectDAO;
+    private TaskDAO taskDAO;
+    private Transformer transformer;
+
+
+    public ProjectTaskServiceImpl(ProjectDAO projectDAO, TaskDAO taskDAO,Transformer transformer) {
+        this.projectDAO = projectDAO;
+        this.taskDAO = taskDAO;
+        this.transformer = transformer;
+    }
+
+    @Override
+    public List<ProjectDTO> getAllProjects(String username) {
+       return projectDAO.findAllProjectsByUsername(username).stream().
+                map(transformer::toprojectDTO).collect(Collectors.toList());
+    }
+
+    @Override
+    public ProjectDTO getProjectDetails(String username, int projectId) {
+        ProjectDTO project = projectDAO.findById(projectId).
+                map(transformer::toprojectDTO).orElseThrow(() -> new EmptyResultDataAccessException(1));
+        if (project.getUsername().matches(username))throw new  AccessDeniedException();
+        return project;
+    }
+
+    @Override
+    public void renameProject(ProjectDTO project) {
+        Project projectEntity = projectDAO.findById(project.getId()).
+                orElseThrow(() -> new EmptyResultDataAccessException(1));
+       if (!projectEntity.getUsername().matches(project.getUsername())) throw  new AccessDeniedException();
+       projectDAO.update(transformer.toProject(project));
+
+
+    }
+
+    @Override
+    public void deleteProject(String username, int projectId) {
+        Project project = projectDAO.findById(projectId).orElseThrow(() -> new EmptyResultDataAccessException(1));
+        if (!project.getUsername().matches(username)) throw new AccessDeniedException();
+        taskDAO.findAllTasksByProjectId(projectId).forEach(task -> taskDAO.deleteById(task.getId()));
+        projectDAO.deleteById(projectId);
+
+    }
+
+    @Override
+    public ProjectDTO createNewProject(ProjectDTO projectDTO) {
+        return transformer.toprojectDTO(projectDAO.save(transformer.toProject(projectDTO)));
+    }
 }
--- src/main/java/lk/ijse/dep9/app/util/Transformer.java
+++ src/main/java/lk/ijse/dep9/app/util/Transformer.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep9.app.util;
 
+import lk.ijse.dep9.app.dto.ProjectDTO;
 import lk.ijse.dep9.app.dto.UserDTO;
+import lk.ijse.dep9.app.entity.Project;
 import lk.ijse.dep9.app.entity.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +22,12 @@ public class Transformer {
 
     public UserDTO touserDTO(User user){
         return mapper.map(user, UserDTO.class);
+    }
+
+    public Project toProject(ProjectDTO dto){
+        return mapper.map(dto, Project.class);
+    }
+    public ProjectDTO toprojectDTO(Project projectEntity){
+        return mapper.map(projectEntity, ProjectDTO.class);
     }
 }
